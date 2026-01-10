c0e145781bfd550746c7eebb9e0b094d00b4cf32
Ilaria Di Capua <ilariadicapua01@gmail.com>
2026-01-10 11:02:39 +0100

    fix: run.py variable error

--- parametric-timer/run.py
+++ parametric-timer/run.py
@@ -19,13 +19,13 @@ lib.add_source_files(ROOT / "test" / "*.vhd")
 tb = lib.entity("tb_timer")
 
 # Test Case 1: 100MHz clock with 100ns delay
-tb.add_config(name="100MHz", generics=dict(clk_freq_hz_g=100e6, delay_ns_g=100000))
+tb.add_config(name="100MHz", generics=dict(clk_freq_hz_g= int(100e6), delay_ns_g=100))
 
 # Test Case 2: 50Mz clock with 200ns delay
-tb.add_config(name="50MHz", generics=dict(clk_freq_hz_g=50e6, delay_ns_g=200000))
+tb.add_config(name="50MHz", generics=dict(clk_freq_hz_g=int(50e6), delay_ns_g=200))
 
 # Test Case 3: 1MHz clock with 10us delay
-tb.add_config(name="1MHz", generics=dict(clk_freq_hz_g=1e6, delay_ns_g=10000000))
+tb.add_config(name="1MHz", generics=dict(clk_freq_hz_g= int(1e6), delay_ns_g=10000))
 
 # Run vunit function
 vu.main()
